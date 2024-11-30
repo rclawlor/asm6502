70d19c260cdb1e82fe17dff7218ccdc4dbce6103
Ronan Lawlor <ronanlawlor2001@gmail.com>
2024-11-30 18:04:27 +0000

    Add error locations when incorrectly parsing numbers

--- src/lexer.rs
+++ src/lexer.rs
@@ -24,13 +24,14 @@ pub enum Token {
 
 pub struct Lexer {
     tokens: Vec<Vec<Token>>,
-    line_idx: usize
+    line_idx: usize,
+    line: String
 }
 
 impl Lexer {
     /// Create a `Lexer` instance
     pub fn new() -> Self {
-        Self { tokens: Vec::new(), line_idx: 1 }
+        Self { tokens: Vec::new(), line_idx: 1, line: String::new() }
     }
 
     /// Lex file and output tokens
@@ -41,6 +42,7 @@ impl Lexer {
                 Err(e) => return Err(LexerError::FileRead(format!("Unable to read file: {}", e)))
             };
             self.line_idx = line_idx;
+            self.line = line.clone();
 
             let mut line_tokens = Vec::new();
             let mut it = line.chars().enumerate().peekable();
@@ -142,16 +144,32 @@ impl Lexer {
 
     /// Parse a hex number from the source file
     fn get_hex_number(&mut self, it: &mut Peekable<Enumerate<Chars<'_>>>) -> Result<u16, LexerError> {
+        let mut number_str = Vec::new();
+        let (start_idx, _) = it.peek().expect("Already checked to have number").clone();
+        while let Some((_, c)) = it.peek() {
+            match c {
+                ' ' | '\n' | ',' => break,
+                _ => {
+                    let (_, c) = it.next().unwrap();
+                    number_str.push(c);
+                }
+            }
+        }
+
         let mut number: u16 = 0;
-        while let Some((char_idx, c)) = it.peek() {
+        let number_len = number_str.len();
+        for c in number_str {
             match c {
                 '0'..='9' | 'a'..='f' | 'A'..='F' => {
-                    let (_, c) = it.next().unwrap();
                     number = (number * 16) + u16::from_str_radix(&c.to_string(), 16)
                         .expect("Number already checked to be within [0-9a-fA-F] range");
+
                 },
-                ' ' | '\n' | ',' => break,
-                _ => return Err(LexerError::InvalidNumber(format!("Invalid hex number"), self.line_idx, *char_idx, 1, "".to_string()))
+                _ => return Err(
+                    LexerError::InvalidNumber(
+                        format!("Invalid hex number"), self.line_idx, start_idx, number_len, self.line.clone()
+                    )
+                )
             }
         }
 
@@ -160,17 +178,32 @@ impl Lexer {
 
     /// Parse a decimal number from the source file
     fn get_decimal_number(&mut self, it: &mut Peekable<Enumerate<Chars<'_>>>) -> Result<u16, LexerError> {
+        let mut number_str = Vec::new();
+        let (start_idx, _) = it.peek().expect("Already checked to have number").clone();
+        while let Some((_, c)) = it.peek() {
+            match c {
+                ' ' | '\n' | ',' => break,
+                _ => {
+                    let (_, c) = it.next().unwrap();
+                    number_str.push(c);
+                }
+            }
+        }
+
         let mut number: u16 = 0;
-        while let Some((char_idx, c)) = it.peek() {
+        let number_len = number_str.len();
+        for c in number_str {
             match c {
                 '0'..='9' => {
-                    let (_, c) = it.next().unwrap();
                     number = (number * 10) + u16::from_str_radix(&c.to_string(), 10)
                         .expect("Number already checked to be within [0-9] range");
-                }
-                ' ' | '\n' | ',' => break,
-                _ => return Err(LexerError::InvalidNumber(format!("Invalid decimal number"), self.line_idx, *char_idx, 1, "".to_string()))
 
+                },
+                _ => return Err(
+                    LexerError::InvalidNumber(
+                        format!("Invalid decimal number"), self.line_idx, start_idx, number_len, self.line.clone()
+                    )
+                )
             }
         }
 
@@ -179,23 +212,32 @@ impl Lexer {
 
     /// Parse a binary number from the source file
     fn get_binary_number(&mut self, it: &mut Peekable<Enumerate<Chars<'_>>>) -> Result<u16, LexerError> {
+        let mut number_str = Vec::new();
+        let (start_idx, _) = it.peek().expect("Already checked to have number").clone();
+        while let Some((_, c)) = it.peek() {
+            match c {
+                ' ' | '\n' | ',' => break,
+                _ => {
+                    let (_, c) = it.next().unwrap();
+                    number_str.push(c);
+                }
+            }
+        }
+
         let mut number: u16 = 0;
-        while let Some((char_idx, c)) = it.peek() {
+        let number_len = number_str.len();
+        for c in number_str {
             match c {
                 '0'..='1' => {
-                    let (_, c) = it.next().unwrap();
-                    number = (number * 2) + c.to_string().parse::<u16>().expect("Number already checked to be within 0-9 range");
-                },
-                '2'..='9' => {
-                    return Err(
-                        LexerError::InvalidNumber(
-                            format!("Binary number can only contain [0-1]"), self.line_idx, *char_idx, 1, "".to_string()
-                        )
-                    )
-                }
-                ' ' | '\n' | ',' => break,
-                _ => return Err(LexerError::InvalidNumber(format!("Invalid binary number"), self.line_idx, *char_idx, 1, "".to_string()))
+                    number = (number * 2) + u16::from_str_radix(&c.to_string(), 2)
+                        .expect("Number already checked to be within [0-1] range");
 
+                },
+                _ => return Err(
+                    LexerError::InvalidNumber(
+                        format!("Invalid binary number"), self.line_idx, start_idx, number_len, self.line.clone()
+                    )
+                )
             }
         }
 
